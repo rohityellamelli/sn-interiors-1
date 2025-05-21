d1df8250afaaae1f4554008bb05699b0a8ab9009
Rohit Yellamelli <rohityellamelli@gmail.com>
2025-05-21 16:02:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Simple scroll animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
